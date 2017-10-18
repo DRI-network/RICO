14d5a49ae667882e216986b94d94ad515f725736
syrohei <syrohei@gmail.com>
2017-10-18 21:21:49 +0900

    complete test sutra for Token issuing processes and fixed contract permission

--- test/RICOToken.js
+++ test/RICOToken.js
@@ -19,7 +19,7 @@ contract('RICOToken', function (accounts) {
     const now = Math.floor(new Date().getTime() / 1000);
 
     const projectOwner = accounts[1]
-    const init = await token.mintable(projectOwner, 1000 * 10 ** decimals, now, {
+    const mintable = await token.mintable(projectOwner, 1000 * 10 ** decimals, now, {
       from: owner
     });
 
@@ -27,15 +27,15 @@ contract('RICOToken', function (accounts) {
 
     const balance = await token.balanceOf(projectOwner)
 
-    assert.strictEqual(balance.toNumber(), 1000 * 10 ** decimals, 'balance of account2 == 1000 * 10 ** decimals')
+    assert.strictEqual(balance.toNumber(), 1000 * 10 ** decimals, 'balance of projectOwner != 1000 * 10 ** decimals')
 
   })
   it("should be disable mint now for projectOwner", async function () {
     const now = Math.floor(new Date().getTime() / 1000);
     const account2 = accounts[2]
-    
+
     const projectOwner = accounts[1]
-    const init = await token.mintable(account2, 1000 * 10 ** decimals, now+22222, {
+    const mintable = await token.mintable(account2, 1000 * 10 ** decimals, now + 22222, {
       from: owner
     });
 
@@ -44,6 +44,70 @@ contract('RICOToken', function (accounts) {
     const balance = await token.balanceOf(account2)
 
     assert.strictEqual(balance.toNumber(), 0, 'assert error balance of account2 != 0')
+
+  })
+  it("should be mintable additional token and mint now for projectOwner", async function () {
+    const now = Math.floor(new Date().getTime() / 1000);
+
+    const projectOwner = accounts[1]
+    const mintable = await token.mintable(projectOwner, 1000 * 10 ** decimals, now, {
+      from: owner
+    });
+
+    const mint = await token.mint(projectOwner)
+
+    const balance = await token.balanceOf(projectOwner)
+
+    assert.strictEqual(balance.toNumber(), 2000 * 10 ** decimals, 'balance of projectOwner != 2000 * 10 ** decimals')
+
+  })
+  it("should be mintable additional token and mint now for projectOwner", async function () {
+    const now = Math.floor(new Date().getTime() / 1000);
+
+    const projectOwner = accounts[1]
+    const mintable1 = await token.mintable(projectOwner, 1000 * 10 ** decimals, now, {
+      from: owner
+    });
+
+    const mintable2 = await token.mintable(projectOwner, 2000 * 10 ** decimals, now, {
+      from: owner
+    });
+
+    const mintable3 = await token.mintable(projectOwner, 3000 * 10 ** decimals, now, {
+      from: owner
+    });
+
+    const mint = await token.mint(projectOwner)
+
+    const balance = await token.balanceOf(projectOwner)
+
+    assert.strictEqual(balance.toNumber(), 8000 * 10 ** decimals, 'balance of projectOwner != 8000 * 10 ** decimals')
+
+  })
+
+  it("should be changed owner by oldOwner", async function () {
+    const now = Math.floor(new Date().getTime() / 1000);
+    const projectOwner = accounts[1]
+    
+    const newOwner = accounts[3]
+
+    const changed = await token.changeOwner(newOwner, {
+      from: owner
+    })
+
+    const mintable1 = await token.mintable(projectOwner, 3000 * 10 ** decimals, now, {
+      from: owner
+    });
+
+    const mintable2 = await token.mintable(projectOwner, 3000 * 10 ** decimals, now, {
+      from: newOwner
+    });
+
+    const mint = await token.mint(projectOwner)
+    
+    const balance = await token.balanceOf(projectOwner)
+
+    assert.strictEqual(balance.toNumber(), 11000 * 10 ** decimals, 'balance of projectOwner != 11000 * 10 ** decimals')
 
   })
 })
