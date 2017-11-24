b34e3ae6fe4c7056a19b8c0da35294f06c9b6a05
syrohei <syrohei@gmail.com>
2017-11-24 13:36:37 +0900

    refine freeza

--- contracts/Freezable.sol
+++ contracts/Freezable.sol
@@ -9,14 +9,13 @@ contract Freezable {
   /**
    * Storage
    */
-
   address owner;
   bool executable;
+  bool cold;
 
   /**
    * Modifier
    */
-
   modifier onlyOwner() {
     require(msg.sender == owner);
     _;
@@ -30,26 +29,29 @@ contract Freezable {
    * @notice Constructor method
    * @dev Constructor is called when contract deployed.
    */
-
   function Freezable() public {
     owner = msg.sender;
     executable = true;
+    cold = false;
   }
 
   /**
    * functions
    */
 
-  /// @notice freeza execute to initializing registry.
-  /// @dev freeza is called by Owner.
-  /// @param  _flag    to be stop all executable function when occurring emergency.
+  /// @notice set to be stop all executable functions temporarily if occurring emergencies.
+  /// @dev pause is called by Owner.
+  /// @param _flag param represent a lock flag value 1 or 0 
   function pause(bool _flag) public onlyOwner() returns (bool) {
+    require(!cold);
     executable = _flag;
     return executable;
   }
-
+  /// @notice Set to be non executable eternally.
+  /// @dev freeza is called by Owner.
   function freeza() public onlyOwner() returns (bool) {
     executable = false;
+    cold = true;
     return executable;
   }
 }
