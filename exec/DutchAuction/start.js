c3e9d080aba47cc60464ca14e19d1d7196a10519
syrohei <syrohei@gmail.com>
2017-12-04 04:03:58 +0900

    Fix #15 refine start execution

--- exec/DutchAuction/start.js
+++ exec/DutchAuction/start.js
@@ -2,13 +2,30 @@ const DutchAuctionPoD = artifacts.require("./PoDs/DutchAuctionPoD.sol")
 
 module.exports = async function (deployer, network, accounts) {
 
+  const blocktime = await getBlock()
   const dap = await DutchAuctionPoD.deployed()
 
-  const now = web3.eth.getBlock(web3.eth.blockNumber).timestamp
-
-  const start = await dap.start(now + 500).catch((err) => {
+  const start = await dap.start(blocktime + 500).catch((err) => {
     console.log(err)
   })
   console.log("transaction tx:", start.tx)
+}
 
+function getBlock() {
+  return new Promise((resolve, reject) => {
+    getBlockNum().then((result) => {
+      web3.eth.getBlock(result, (err, data) => {
+        const timestamp = data.timestamp
+        resolve(timestamp)
+      })
+    })
+  })
+}
+
+function getBlockNum() {
+  return new Promise((resolve, reject) => {
+    web3.eth.getBlockNumber((err, result) => {
+      resolve(result)
+    })
+  })
 }
