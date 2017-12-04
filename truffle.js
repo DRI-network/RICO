bbe297acc3eef4edb88b3e84384c82714165fede
syrohei <syrohei@gmail.com>
2017-12-05 03:58:11 +0900

    fix config.js

--- truffle.js
+++ truffle.js
@@ -15,5 +15,39 @@ rinkeby = new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/')
 //console.log(rinkebyProvider.address)
 
 module.exports = {
- 
+  networks: {
+    development: {
+      host: "localhost",
+      port: 9545,
+      network_id: "*", // Match test network id
+      gas: 4642056,
+      gasPrice: 10000000000
+    },
+    testnet: {
+      host: "192.168.0.103",
+      port: 8545,
+      network_id: 3, // Match ropsten network id
+      gas: 4700036,
+      gasPrice: 15000000000
+    },
+    ropsten: {
+      provider: ropsten,
+      network_id: 3, // Match ropsten network id
+      gas: 4700036,
+      gasPrice: 26000000000
+    },
+    rinkeby: {
+      provider: rinkeby,
+      network_id: 4,
+      gas: 4700036,
+      gasPrice: 15000000000
+    },
+    mainnet: {
+      host: "10.23.122.2",
+      port: 8545,
+      network_id: 1, // Match main network id
+      gas: 7000000, // Gaslimit based on latestblock
+      gasPrice: 30000000000
+    }
+  }
 };
