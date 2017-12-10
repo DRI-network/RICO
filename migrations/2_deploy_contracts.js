09029364fd94a965ed0ccf845073c3eb89673dc5
syrohei <syrohei@gmail.com>
2017-12-11 00:10:26 +0900

    fix badge for slack and migrate process fixed.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
 const LauncherSample = artifacts.require("./LauncherSample.sol");
 const RICO = artifacts.require("./RICO.sol");
 const RICOToken = artifacts.require("./RICOToken.sol");
-
+const TokenMintPoD = artifacts.require("./PoDs/TokenMintPoD.sol")
 const SimplePoD = artifacts.require("./PoDs/SimplePoD.sol")
 const KaitsukePoD = artifacts.require("./PoDs/KaitsukePoD.sol")
 const DutchAuctionPoD = artifacts.require("./PoDs/DutchAuctionPoD.sol")
@@ -10,7 +10,9 @@ const totalTokenSupply = 400000 * 10 ** 18; // set maximum supply to 400,000.
 const tobTokenSupply = totalTokenSupply * 3 / 100
 const tobWeiLimit = 100 * 10 ** 18
 const podTokenSupply = totalTokenSupply * 20 / 100
-const podWeiLimit = 1000 * 10 ** 18
+const podWeiLimit = 100 * 10 ** 18
+
+const firstSupply = totalTokenSupply * 30 / 100;
 const decimals = 18;
 
 
@@ -21,6 +23,8 @@ module.exports = async function (deployer, network, accounts) {
   deployer.deploy(LauncherSample).then(() => {
     return deployer.deploy(RICO)
   }).then(() => {
+    return deployer.deploy(TokenMintPoD)
+  }).then(() => {
     return deployer.deploy(RICOToken)
   }).then(() => {
     return deployer.deploy(KaitsukePoD)
@@ -28,44 +32,41 @@ module.exports = async function (deployer, network, accounts) {
     return deployer.deploy(SimplePoD)
   }).then(async() => {
     // certifiers
-    const addresses = [
-      accounts[0],
-      accounts[1],
-      accounts[2]
-    ]
+    projectOwner = accounts[0]
+    tobAccount = accounts[1]
 
-    const rico = await RICO.deployed()
-    const token = await RICOToken.deployed()
-    const launcher = await LauncherSample.deployed()
-    const tob = await KaitsukePoD.deployed()
-    const pod = await SimplePoD.deployed()
+    rico = await RICO.new()
+    token = await RICOToken.new()
+    launcher = await LauncherSample.new()
+    tob = await KaitsukePoD.new()
+    pod = await SimplePoD.new()
+    mint1 = await TokenMintPoD.new()
 
-    const pods = [
+    pods = [
       tob.address,
       pod.address,
+      mint1.address
     ]
 
-    const setConfigToB = await tob.setConfig(decimals, tobTokenSupply, tobWeiLimit, accounts[1])
+    const setConfigToB = await tob.setConfig(decimals, tobTokenSupply, tobWeiLimit, tobAccount)
     const changeOwnerToB = await tob.transferOwnership(rico.address)
 
     const setConfigPoD = await pod.setConfig(decimals, podTokenSupply, podWeiLimit)
     const changeOwnerPoD = await pod.transferOwnership(rico.address)
 
+    const now = web3.eth.getBlock(web3.eth.blockNumber).timestamp
+
+    const setConfigMint1 = await mint1.setConfig(projectOwner, 72000, firstSupply)
+    const changeOwnerMint1 = await mint1.transferOwnership(rico.address)
+
     // changing owner to owner to rico.
     const changeOwnerToken = await token.transferOwnership(rico.address)
-
     const changeOwnerRICO = await rico.transferOwnership(launcher.address)
 
     //initializing launcher.
-    const init = await launcher.init(rico.address, totalTokenSupply, token.address, pods, {
-      from: accounts[0]
-    }).catch((err) => {
-      console.log(err)
-    })
+    const init = await launcher.init(rico.address, totalTokenSupply, token.address, pods)
 
     //setup launcher
-    const setup = await launcher.setup(accounts[0], {
-      from: accounts[0]
-    });
+    const setup = await launcher.setup(accounts[0]);
   });
 }
